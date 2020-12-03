de58a3bf2c28800a490e2c19c63c34d670f16118
Jason Xu <xuqiang2088@gmail.com>
2020-12-03 01:14:23 -0800

    database and migrations

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'JasonXu',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'Nov 29, 2020'
-    },
-    {
-        'author': 'Lebron James',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'Nov 30, 2020'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
